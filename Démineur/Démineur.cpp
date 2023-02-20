d612e10576892e9e0022a39bfde231afc55f4f6b
rboisseau <rboisseau@GCFRLY239>
2023-02-20 15:14:29 +0100

    Commit de lux

--- Démineur/Démineur.cpp
+++ Démineur/Démineur.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
+void Board (){
+    int divide = 0;
+    int board[] = { 0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9,
+                    0,1,2,3,4,5,6,7,8,9, };
+    int length = sizeof(board) / sizeof(board[0]);
+    for (int i = 0; i < length; i++) {
+        //printf("%d ", x[i]);
+        printf("Chiffre: %d", i / 10);
+    }
+}
+
 int main()
 {
+
     std::cout << "Hello World!\n";
+
+    Board();
+
 }
 
