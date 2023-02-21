5a608fe543e36c1b281faa4bffbb55ad3ee14501
rboisseau <rboisseau@GCFRLY239>
2023-02-21 10:00:04 +0100

    Update -> ver 21.02.2023.0.2.2

--- Démineur/Démineur.cpp
+++ Démineur/Démineur.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <stdlib.h>
 #define size 10
 
 void printBoard(char board[], int length){
-    int divide = 1;
+    int divide = 0;
     for (int i = 0; i < length; i++) {
         if (i/size == divide){
             divide++;
@@ -26,29 +26,39 @@ void printBoard(char board[], int length){
 
 int main()
 {
-    char board[size * size];
+    //The sense of life. https://youtu.be/dQw4w9WgXcQ
+    std::cout << "Demineur of Bebou UWU!\n";
 
-	int i, n;
+    //Boards init
+    char trappedBoard[size * size];
+	char playableBoard[size * size];
+    
+    //Random values init
+	int i, random;
+    srand(time(NULL));
 
-	srand(time(NULL));
-
-    std::cout << "Démineur de Bebou UWU!\n";
-
-    for (int i = 0; i < size * size; i++) {
-        board[i] = 0;
+    //Boards creation
+    for (int i = 0; i < size * size; i++) { 
+		trappedBoard[i] = 0;
+		playableBoard[i] = 0;
     }
     
-
-	for (i = 1; i <= 10; i++) {
-		n = rand() % 100;
-		printf("%d\n", n);
-        if (board[n] == 1){
+    //Random Creation
+    for (i = 1; i <= 10; i++) {
+		random = rand() % 100;
+        printf("%d\n", random);
+        if (trappedBoard[random] == 1) {
             i--;
         }
-        board[n] = 1;
-	}
+        trappedBoard[random] = 1;
+    }
 
-    printBoard(board, size * size);
+    //Boards print
+    printf("\nUntrapped :\n");
+	printBoard(playableBoard, size * size);
+
+    printf("\n\nTrapped :\n");
+    printBoard(trappedBoard, size * size);
 
 }
 
