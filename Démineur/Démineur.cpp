63611dc38b140fe74644ba62fa15476411a687ba
rboisseau <rboisseau@GCFRLY239>
2023-02-24 15:26:26 +0100

    Update -> ver 24.02.23.0.5

--- Démineur/Démineur.cpp
+++ Démineur/Démineur.cpp
@@ -6,7 +6,9 @@ void printNumbers(int clue);
 int play(char trappedBoard[], char playableBoard[]);
 void printBoard(char board[], int length);
 int playVerif(char trappedBoard[], char playableBoard[], int x, int y, int flag);
-void showArea(char trappedBoard[], char playableBoard[], int x, int y, int length);
+void showArea(char trappedBoard[], char playableBoard[], int length);
+void bombCreation(char trappedBoard[], int x, int y);
+void boardCreation(char trappedBoard[], char playableBoard[]);
 int main();
 void red();
 void white();
@@ -60,16 +62,91 @@ void setColor(char board[], int i) {
 void printNumbers(int clue) {
     if (clue == 0) {
         printf("\n   ");
+	}
+    else if (clue == 1){
+		printf("\n  %d", clue);
     }
-    else if (clue < 10) {
-        printf("\n  %d", clue);
+	else if (clue < 10) {
+		printf(" %d\n  %d", clue-1, clue);
+	}
+	else if (clue >= 10) {
+		printf(" %d\n %d", clue-1, clue);
+	}
+}
+
+void boardCreation(char trappedBoard[], char playableBoard[]){
+	//Boards creation
+	for (int i = 0; i < size * size; i++) {
+		trappedBoard[i] = 10;
+		playableBoard[i] = 13;
+	}
+}
+
+void bombCreation(char trappedBoard[], int x , int y) {
+	//Random values init
+	int i, random;
+	srand(time(NULL));
+
+    //Random Creation Bomb
+    for (int i = 1; i <= (size * size) / 10; i++) {
+        int bombNumber = size * size;
+        random = rand() % bombNumber;
+        if (trappedBoard[random] == 11) {
+            i--;
+        }
+        trappedBoard[random] = 11;
     }
-    else if (clue >= 10) {
-        printf("\n %d", clue);
+
+    //Verif Number Bomb
+    for (int i = 0; i < size * size; i++)
+    {
+        int numberBomb = 0;
+        if (trappedBoard[i] != 11) {
+
+            //Verify Bomb Right
+            if (trappedBoard[i + 1] == 11 && (i + 1) % size != 0) {
+                numberBomb++;
+            }
+            //Verify Bomb Left
+            if (trappedBoard[i - 1] == 11 && (i - 1) % size != size - 1) {
+                numberBomb++;
+            }
+            //Verify Bomb Up
+            if (trappedBoard[i - size] == 11 && i >= size) {
+                numberBomb++;
+            }
+            //Verify Bomb Down
+            if (trappedBoard[i + size] == 11 && i <= (size * size) - size) {
+                numberBomb++;
+            }
+
+            //Verify Bomb Right Up
+            if (trappedBoard[i - (size - 1)] == 11 && i >= size && (i + 1) % size != 0 && i != 0) {
+                numberBomb++;
+            }
+            //Verify Bomb Left Up
+            if (trappedBoard[i - (size + 1)] == 11 && i >= size && (i - 1) % size != size - 1) {
+                numberBomb++;
+            }
+            //Verify Bomb Right Down
+            if (trappedBoard[i + (size + 1)] == 11 && i <= (size * size) - size && (i + 1) % size != 0) {
+                numberBomb++;
+            }
+            //Verify Bomb Left Down
+            if (trappedBoard[i + (size - 1)] == 11 && i <= (size * size) - size && (i - 1) % size != size - 1) {
+                numberBomb++;
+            }
+
+            //Verify if is Bomb
+            if (numberBomb != 0)
+            {
+                trappedBoard[i] = numberBomb;
+            }
+        }
     }
 }
 
-void showArea(char trappedBoard[], char playableBoard[], int x, int y, int length) {
+void showArea(char trappedBoard[], char playableBoard[], int length) {
     int isO = 1;
     while (isO == 1){
         isO = 0;
@@ -118,10 +195,46 @@ void showArea(char trappedBoard[], char playableBoard[], int x, int y, int lengt
                     playableBoard[i] = trappedBoard[i];
 					isO = 1;
 				}
+
 			}
 		}
     }
     
+}
+
+void prePlay(char trappedBoard[], char playableBoard[]) {
+    int x;
+    int y;
+
+	printf("\nIndiquer une colonne entre (1 et %d): ", size);
+	scanf_s("%d", &x);
+	printf("\nIndiquer une ligne entre (1 et %d): ", size);
+	scanf_s("%d", &y);
+
+	while (x > size || x < 1 || y > size || y < 1) {
+		printf("\nValeurs non valide\n");
+		printf("\nIndiquer une colonne entre (1 et %d): ", size);
+		scanf_s("%d", &x);
+		printf("\nIndiquer une ligne entre (1 et %d): ", size);
+		scanf_s("%d", &y);
+    }
+
+    boardCreation(trappedBoard, playableBoard);
+    bombCreation(trappedBoard, x, y);
+
+    while (trappedBoard[(x + (y - 1) * size) - 1] != 10) {
+		boardCreation(trappedBoard, playableBoard);
+		bombCreation(trappedBoard, x, y);
+    }
+
+    playableBoard[(x + (y - 1) * size) - 1] = 10;
+
+    showArea(trappedBoard, playableBoard, size * size);
+
+    //Boards print
+    printf("\nMinesweeper :\n");
+    printBoard(playableBoard, size * size);
+
 }
 
 int play(char trappedBoard[], char playableBoard[]) {
@@ -135,11 +248,18 @@ int play(char trappedBoard[], char playableBoard[]) {
     printf("\nVoulez-vous mettre un drapeau ? yes = 1 or no = 0\n");
     scanf_s("%d", &answer);
 
-    printf("\nIndiquer une colonne: ");
-    scanf_s("%d", &x);
-    printf("\nIndiquer une ligne: ");
-    scanf_s("%d", &y);
-    printf("\nValeur x: %d, Valeur y: %d", x, y);
+	printf("\nIndiquer une colonne entre (1 et %d): ", size);
+	scanf_s("%d", &x);
+	printf("\nIndiquer une ligne entre (1 et %d): ", size);
+	scanf_s("%d", &y);
+
+	while (x > size || x < 1 || y > size || y < 1) {
+		printf("\nValeurs non valide\n");
+		printf("\nIndiquer une colonne entre (1 et %d): ", size);
+		scanf_s("%d", &x);
+		printf("\nIndiquer une ligne entre (1 et %d): ", size);
+		scanf_s("%d", &y);
+	}
 
     if (answer == 1) {
         flag = 1;
@@ -150,13 +270,11 @@ int play(char trappedBoard[], char playableBoard[]) {
 
     isBomb = playVerif(trappedBoard, playableBoard, x, y, flag);
 
-    showArea(trappedBoard, playableBoard, x, y, size*size);
+    showArea(trappedBoard, playableBoard, size*size);
 
-    printf("\nUntrapped :\n");
+	//Boards print
+    printf("\nMinesweeper :\n");
     printBoard(playableBoard, size * size);
-
-    printf("\n\nTrapped :\n");
-    printBoard(trappedBoard, size * size);
 
     return isBomb;
 }
@@ -179,17 +297,19 @@ void printBoard(char board[], int length) {
 
             }
         }
+		
         if (i / size == divide) {
             divide++;
             if (board[i] == 10) {
                 printNumbers(divide);
                 white();
-                printf("  O");
+                printf("   ");
             }
             else if (board[i] == 11) {
                 printNumbers(divide);
                 cyan();
-                printf("  X");
+				printf("  X");
+				white();
             }
             else if (board[i] == 12) {
                 printNumbers(divide);
@@ -200,7 +320,7 @@ void printBoard(char board[], int length) {
             else if (board[i] == 13) {
                 printNumbers(divide);
                 white();
-                printf("  ?");
+                printf("  .");
             }
             if (board[i] < 10)
             {
@@ -213,7 +333,7 @@ void printBoard(char board[], int length) {
         else {
             if (board[i] == 10) {
                 white();
-                printf("  O");
+                printf("   ");
             }
             else if (board[i] == 11) {
                 cyan();
@@ -227,7 +347,7 @@ void printBoard(char board[], int length) {
             }
             else if (board[i] == 13) {
                 white();
-                printf("  ?");
+                printf("  .");
             }
             if (board[i] < 10)
             {
@@ -236,14 +356,42 @@ void printBoard(char board[], int length) {
                 white();
             }
         }
+		if (i == (size * size) - 1) {
+			for (int j = 0; j <= size; j++) {
+				if (j == 0) {
+					printf(" %d\n   ",size);
+				}
+				else if (j < 10) {
+					printf("  %d", j);
+				}
+				else if (j >= 10) {
+					printf(" %d", j);
+				}
 
+			}
+		}
     }
 }
 
 int playVerif(char trappedBoard[], char playableBoard[], int x, int y, int flag) {
-    if (flag == 1) {
+    int victory = 0;
+
+    if (flag == 1 && playableBoard[(x + (y - 1) * size) - 1] == 13) {
         playableBoard[(x + (y - 1) * size) - 1] = 12;
-        return 0;
+		for (int i = 0; i < size * size; i++)
+		{
+			if (playableBoard[i] == 12 || playableBoard[i] == 13) {
+				victory++;
+			}
+		}
+		if (victory == (size * size) / 10) {
+			return 2;
+		}
+		else {
+			return 0;
+		}
+    } else if(flag == 1) {
+		return 0;
     }
     else {
         if (trappedBoard[(x + (y - 1) * size) - 1] == 11) {
@@ -253,105 +401,43 @@ int playVerif(char trappedBoard[], char playableBoard[], int x, int y, int flag)
         else {
             printf("\t\nOnly luck");
             playableBoard[(x + (y - 1) * size) - 1] = trappedBoard[(x + (y - 1) * size) - 1];
-            return 0;
+            for (int i = 0; i < size * size; i++)
+            {
+                if (playableBoard[i] == 12 || playableBoard[i] == 13){
+                    victory++;
+                }
+            }
+            if (victory == (size * size)/10){
+                return 2;
+            }else{
+				return 0;
+            }
         }
+        
     }
 
 }
 
-int main()
-{
+int main() {
     //The sense of life. https://youtu.be/dQw4w9WgXcQ
     std::cout << "Minesweeper of BeboUwU!\n";
 
     //init win/lose
-    int booleanWin = 0;
     int booleanLoose = 0;
 
     //Boards init
     char trappedBoard[size * size];
     char playableBoard[size * size];
 
-    //Random values init
-    int i, random;
-    srand(time(NULL));
+    prePlay(trappedBoard, playableBoard);
 
-    //Coords Init
-    int x = 0;
-    int y = 0;
-
-    //Boards creation
-    for (int i = 0; i < size * size; i++) {
-        trappedBoard[i] = 10;
-        playableBoard[i] = 13;
-    }
-
-    //Random Creation Bomb
-    for (int i = 1; i <= (size * size) / 10; i++) {
-        int bombNumber = size * size;
-        random = rand() % bombNumber;
-        if (trappedBoard[random] == 11) {
-            i--;
-        }
-        trappedBoard[random] = 11;
-    }
-
-    //Verif Number Bomb
-    for (int i = 0; i < size * size; i++)
-    {
-        int numberBomb = 0;
-        if (trappedBoard[i] != 11) {
-
-            //Verify Bomb Right
-            if (trappedBoard[i + 1] == 11 && (i + 1) % size != 0) {
-                numberBomb++;
-            }
-            //Verify Bomb Left
-            if (trappedBoard[i - 1] == 11 && (i - 1) % size != size-1) {
-                numberBomb++;
-            }
-            //Verify Bomb Up
-            if (trappedBoard[i - size] == 11 && i >= size) {
-                numberBomb++;
-            }
-            //Verify Bomb Down
-            if (trappedBoard[i + size] == 11 && i <= (size * size) - size) {
-                numberBomb++;
-            }
-
-            //Verify Bomb Right Up
-            if (trappedBoard[i - (size - 1)] == 11 && i >= size && (i + 1) % size != 0 && i != 0) {
-                numberBomb++;
-            }
-            //Verify Bomb Left Up
-            if (trappedBoard[i - (size + 1)] == 11 && i >= size && (i - 1) % size != size - 1) {
-                numberBomb++;
-            }
-            //Verify Bomb Right Down
-            if (trappedBoard[i + (size + 1)] == 11 && i <= (size * size) - size && (i + 1) % size != 0) {
-                numberBomb++;
-            }
-            //Verify Bomb Left Down
-            if (trappedBoard[i + (size - 1)] == 11 && i <= (size * size) - size && (i - 1) % size != size - 1) {
-                numberBomb++;
-            }
-
-            //Verify if is Bomb
-            if (numberBomb != 0)
-            {
-                trappedBoard[i] = numberBomb;
-            }
-        }
-    }
-    //Boards print
-    printf("\nUntrapped :\n");
-    printBoard(playableBoard, size * size);
-
-    printf("\n\nTrapped :\n");
-    printBoard(trappedBoard, size * size);
-
-    while (booleanLoose == 0 && booleanWin == 0) {
+    while (booleanLoose == 0) {
         booleanLoose = play(trappedBoard, playableBoard);
     }
-    printf("\n\nPedu");
+    if (booleanLoose == 2){
+		printf("\n\Ganer");
+    }
+	else if (booleanLoose == 1) {
+		printf("\n\nPedu");
+    }
 }
